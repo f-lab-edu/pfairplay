72a9f57166dc9f966069114434a92e67defc2a6a
dgryoo <59459120+dgryoo@users.noreply.github.com>
2024-04-22 20:07:19 +0900

    chore : Apply docker, github workflow
    * chore : Modify ignore
    
    * chore : Use H2 DB for Test
    
    * chore : Modify env variable name
    
    * chore : Apply docker
    
    * chore : Add github action workflow
    
    * chore : Disable docker-build-deploy workflow

--- api-server/src/test/java/api/member/MemberControllerIntegrationTest.java
+++ api-server/src/test/java/api/member/MemberControllerIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 public class MemberControllerIntegrationTest {
 
     private final MockMvc mockMvc;
